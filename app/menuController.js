b58e23225cfd0f01d022482aa8e9aceb13aad875
hania <hania.rudy@gmail.com>
2017-04-07 18:24:09 +0200

    add toppings, search and filter options

--- app/menuController.js
+++ app/menuController.js
@@ -1,31 +1,16 @@
-'use strict';
+app.controller('pizzaController', [
+    '$scope',
+    function ($scope) {
+        $scope.model = {
+            title: 'Pizza Builder',
+            availableToppings: ['Cheese', 'Pepperoni', 'Bacon', 'Pineapple', 'Sausage', 'Ham', 'Chicken', 'Mushrooms', 'Onion', 'Olives', 'Green Peppers'],
+            toppings: []
+        };
 
-app.controller('menuController', ['$scope', function ($scope) {
-    $scope.model = {
-        title: 'Our Menu'
-    };
+        $scope.addTopping = function (topping) {
+            $scope.model.toppings.push(topping);
+            $scope.model.search = null;
+        }
+    }
 
-    $scope.mainDish = [{
-        name: 'Cheese Pizza',
-        price: 11
-    }, {
-        name: 'Pepperoni Pizza',
-        price: 12
-    }, {
-        name: 'Margherita Pizza',
-        price: 12
-    }, {
-        name: 'BBQ Chicken Pizza',
-        price: 12
-    }, {
-        name: 'Combo Pizza',
-        price: 12
-    }];
-
-    $scope.changeMainDish = function (item) {
-        $scope.mainDish.name = item.name;
-        $scope.mainDish.price = item.price;
-    };
-
-}
 ]);
