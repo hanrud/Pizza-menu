18aa1e0176a677a413814ff08249b252a3ba2a6d
hania <hania.rudy@gmail.com>
2017-04-07 16:14:23 +0200

    add information about order

--- app/menuController.js
+++ app/menuController.js
@@ -1,14 +1,31 @@
+'use strict';
+
 app.controller('menuController', ['$scope', function ($scope) {
     $scope.model = {
         title: 'Our Menu'
     };
+
+    $scope.mainDish = [{
+        name: 'Cheese Pizza',
+        price: 11
+    }, {
+        name: 'Pepperoni Pizza',
+        price: 12
+    }, {
+        name: 'Margherita Pizza',
+        price: 12
+    }, {
+        name: 'BBQ Chicken Pizza',
+        price: 12
+    }, {
+        name: 'Combo Pizza',
+        price: 12
+    }];
+
     $scope.changeMainDish = function (item) {
-        $scope.model.mainDish = item;
+        $scope.mainDish.name = item.name;
+        $scope.mainDish.price = item.price;
     };
-    $scope.$watch('model.mainDish', function (newValue, oldValue) {
-        if (newValue === 'BBQ Chicken Pizza') {
-            alert('You have selected the BBQ Chicken Pizza!');
-        }
-    })
+
 }
 ]);
