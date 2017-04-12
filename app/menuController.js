6c7645f40bc45c11a52f17618aa2dcae707eb2b2
hanrud <hania.rudy@gmail.com>
2017-04-12 17:59:15 +0200

    merge earlier options

--- app/menuController.js
+++ app/menuController.js
@@ -1,11 +1,37 @@
-app.controller('pizzaController', [
+'use strict';
+
+app.controller('menuController', [
     '$scope',
     function ($scope) {
+
+        $scope.changeMainDish = function (item) {
+            $scope.mainDish.name = item.name;
+            $scope.mainDish.price = item.price;
+        };
         $scope.model = {
             title: 'Pizza Builder',
             availableToppings: ['Cheese', 'Pepperoni', 'Bacon', 'Pineapple', 'Sausage', 'Ham', 'Chicken', 'Mushrooms', 'Onion', 'Olives', 'Green Peppers'],
             toppings: []
         };
+
+        $scope.mainDish = [{
+            name: 'Cheese Pizza',
+            price: 11
+        }, {
+            name: 'Pepperoni Pizza',
+            price: 12
+        }, {
+            name: 'Margherita Pizza',
+            price: 12
+        }, {
+            name: 'BBQ Chicken Pizza',
+            price: 12
+        }, {
+            name: 'Combo Pizza',
+            price: 12
+        }];
+
+
 
         $scope.addTopping = function (topping) {
             $scope.model.toppings.push(topping);
@@ -14,3 +40,5 @@ app.controller('pizzaController', [
     }
 
 ]);
+
+
